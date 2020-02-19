1fcd8d28673fc609c2eee27525e337cac4df0789
Miguel Pardal <miguel.pardal@tecnico.ulisboa.pt>
2020-02-19 23:42:37 +0000

    Add properties file with configuration and code to load it

--- README.md
+++ README.md
@@ -1,8 +1,15 @@
 # Java application example
 
-This is a (very) simple Java application.
+This is a simple Java application
+that loads configuration values from a property file in resources.
 
-It just says Hi, receives and prints the command line arguments, and says Bye!
+The properties file follows the name=value syntax,
+explained in the [wikipedia entry](http://en.wikipedia.org/wiki/.properties).
+
+The min and max values are simple properties.
+The workDir property value is set using a Maven defined property, using the ${work.dir} syntax.
+The ${} placeholder is replaced by the property value.
+
 
 
 ## Maven instructions
@@ -17,6 +24,12 @@ To compile the source code:
 
 ```
 mvn compile
+```
+
+To compile and copy resources to the output directory   (compare the properties file in src/main with the file in target/classes/ after executing):
+
+```
+mvn package
 ```
 
 To run the program using _exec_ plugin:
--- src/main/java/mypackage/MyClass.java
+++ src/main/java/mypackage/MyClass.java
@@ -1,5 +1,9 @@
 package mypackage;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 
 public class MyClass {
 
@@ -15,6 +19,33 @@ public class MyClass {
             System.out.printf("arg[%d] = %s%n", i, args[i]);
         }
 
+        System.out.println();
+
+        // load configuration properties
+        try {
+            InputStream inputStream = MyClass.class.getClassLoader().getResourceAsStream("config.properties");
+            // variant for non-static methods:
+            // InputStream inputStream = getClass().getClassLoader().getResourceAsStream("config.properties");
+
+            Properties properties = new Properties();
+            properties.load(inputStream);
+
+            System.out.printf("Loaded %d properties%n", properties.size());
+
+            String minValue = properties.getProperty("min");
+            System.out.printf("min: %s%n", minValue);
+
+            String maxValue = properties.getProperty("max");
+            System.out.printf("max: %s%n", maxValue);
+
+            String workDirValue = properties.getProperty("workDir");
+            System.out.printf("workDir: %s%n", workDirValue);
+
+        } catch (IOException e) {
+            System.out.printf("Failed to load configuration: %s%n", e);
+        }
+
+        System.out.println();
 
         System.out.println("Bye!");
     }
