5e0f9633a516ea4d4e71d9fe74621f9c998bf440
Miguel Pardal <miguel.pardal@tecnico.ulisboa.pt>
2021-03-05 18:31:06 +0000

    Indent with tabs

--- src/main/java/mypackage/MyClass.java
+++ src/main/java/mypackage/MyClass.java
@@ -7,47 +7,47 @@ import java.util.Properties;
 
 public class MyClass {
 
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hi!");
+	public static void main(String[] args) throws Exception {
+		System.out.println("Hi!");
 
-        System.out.println();
+		System.out.println();
 
-        // receive arguments
-        System.out.printf("Received %d arguments%n", args.length);
-        // print input arguments
-        for (int i=0; i < args.length; i++) {
-            System.out.printf("arg[%d] = %s%n", i, args[i]);
-        }
+		// receive arguments
+		System.out.printf("Received %d arguments%n", args.length);
+		// print input arguments
+		for (int i=0; i < args.length; i++) {
+			System.out.printf("arg[%d] = %s%n", i, args[i]);
+		}
 
-        System.out.println();
+		System.out.println();
 
-        // load configuration properties
-        try {
-            InputStream inputStream = MyClass.class.getClassLoader().getResourceAsStream("config.properties");
-            // variant for non-static methods:
-            // InputStream inputStream = getClass().getClassLoader().getResourceAsStream("config.properties");
+		// load configuration properties
+		try {
+			InputStream inputStream = MyClass.class.getClassLoader().getResourceAsStream("config.properties");
+			// variant for non-static methods:
+			// InputStream inputStream = getClass().getClassLoader().getResourceAsStream("config.properties");
 
-            Properties properties = new Properties();
-            properties.load(inputStream);
+			Properties properties = new Properties();
+			properties.load(inputStream);
 
-            System.out.printf("Loaded %d properties%n", properties.size());
+			System.out.printf("Loaded %d properties%n", properties.size());
 
-            String minValue = properties.getProperty("min");
-            System.out.printf("min: %s%n", minValue);
+			String minValue = properties.getProperty("min");
+			System.out.printf("min: %s%n", minValue);
 
-            String maxValue = properties.getProperty("max");
-            System.out.printf("max: %s%n", maxValue);
+			String maxValue = properties.getProperty("max");
+			System.out.printf("max: %s%n", maxValue);
 
-            String workDirValue = properties.getProperty("workDir");
-            System.out.printf("workDir: %s%n", workDirValue);
+			String workDirValue = properties.getProperty("workDir");
+			System.out.printf("workDir: %s%n", workDirValue);
 
-        } catch (IOException e) {
-            System.out.printf("Failed to load configuration: %s%n", e);
-        }
+		} catch (IOException e) {
+			System.out.printf("Failed to load configuration: %s%n", e);
+		}
 
-        System.out.println();
+		System.out.println();
 
-        System.out.println("Bye!");
-    }
+		System.out.println("Bye!");
+	}
 
 }
